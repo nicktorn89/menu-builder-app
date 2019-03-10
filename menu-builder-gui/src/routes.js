d9c9f5d883edb528fccd3e254fbf7be3ac9cfc4f
nicktorn89 <nicktorn89@gmail.com>
2019-03-10 17:40:35 +0300

    feat: add code splitting with lazy and Suspense
    - Add react-preloaders for fallback to Suspense

--- menu-builder-gui/src/routes.js
+++ menu-builder-gui/src/routes.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Circle } from 'react-preloaders';
 
-import Home from './modules/Home/Home.jsx';
-import Stats from './modules/Stats.jsx';
-import BuildMenu from './modules/BuildMenu/BuildMenu.jsx';
-import AddDish from './modules/AddDish/AddDish.jsx';
-import AddSubDish from './modules/AddSubDish/AddSubDish.jsx';
-import FoodStore from './modules/FoodStore/FoodStore.jsx';
-import BudgetSettings from './modules/BudgetSettings.jsx';
+const Home = lazy(() => import('./modules/Home/Home.jsx'));
+const Stats = lazy(() => import('./modules/Stats.jsx'));
+const BuildMenu = lazy(() => import('./modules/BuildMenu/BuildMenu.jsx'));
+const AddDish = lazy(() => import('./modules/AddDish/AddDish.jsx'));
+const AddSubDish = lazy(() => import('./modules/AddSubDish/AddSubDish.jsx'));
+const FoodStore = lazy(() => import('./modules/FoodStore/FoodStore.jsx'));
+const BudgetSettings = lazy(() => import('./modules/BudgetSettings.jsx'));
 
 class Routes extends Component {
   render() {
     return(
       <Router>
-        <div>
-          <Route exact path="/" component={Home} />
-          <Route path="/build-menu" component={BuildMenu} />
-          <Route path="/add-dish" component={AddDish} />
-          <Route path="/add-sub-dish" component={AddSubDish} />
-          <Route path="/food-store" component={FoodStore} />
-          <Route path="/budget-settings" component={BudgetSettings} />
-          <Route path="/stats" component={Stats} />
-        </div>
+        <Suspense fallback={<Circle />}>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/build-menu" component={BuildMenu} />
+            <Route path="/add-dish" component={AddDish} />
+            <Route path="/add-sub-dish" component={AddSubDish} />
+            <Route path="/food-store" component={FoodStore} />
+            <Route path="/budget-settings" component={BudgetSettings} />
+            <Route path="/stats" component={Stats} />
+          </div>
+        </Suspense>
       </Router>
     );
   }
